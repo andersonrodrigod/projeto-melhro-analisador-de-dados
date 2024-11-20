1b2c8e3f80908fa5f74a83a6e7ed13a8047b2de3
ANDERSON RODRIGO <99427400+andersonrodrigod@users.noreply.github.com>
2024-11-20 13:53:17 -0300

    inicio de tudo

---
+++ analisador_texto_arquivo/app.py
@@ -0,0 +1,18 @@
+# Objetivo: Praticar manipulação de arquivos, strings e processamento de dados.
+
+#Descrição: Crie um programa que leia um arquivo de texto e realize as seguintes operações:
+'''
+Contagem de palavras.
+Contagem de frases.
+Identificação de palavras mais frequentes.
+Extração de palavras únicas e ordenação.
+Exibição de estatísticas básicas, como o número de caracteres, palavras e frases.
+Skills envolvidas:
+
+Manipulação de arquivos (leitura e escrita).
+Contagem e análise de strings.
+Uso de estruturas de dados básicas (listas, dicionários).
+Desafio extra:
+
+Adicione a funcionalidade de limpar o texto (remover pontuação, palavras de parada como "e", "a", "o").
+'''
